1ad55f23787d2b08a44cbee685cd5005c20231c8
cmc33 <cmcguigan33@gmail.com>
2015-08-11 15:18:46 -0400

    adds current weather api

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+#require_relative './key.rb'
+require 'pry'
+require 'json'
+require 'open-uri'
+require_relative "../key.rb"
+require 'forecast_io'
+require_relative '../lib/current'
+require_relative '../lib/home'
+require_relative '../lib/forecast'
+  Home.new
